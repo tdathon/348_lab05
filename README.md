024d633b5af7edfe36808922453281a68e92efd6
tdathon <tdathon@ku.edu>
2023-10-15 20:45:17 -0500

    Fix line and add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+For problem 1, add a sales.txt file to the local directory and input the name of the file (i.e. "sales.txt" for the example) into the program.
+
+For problem 2, simply input a score value.
--- problem01/problem1.c
+++ problem01/problem1.c
@@ -105,9 +105,6 @@ int main() {
 	}
 	printf("\n");
 
-	// int numElements = sizeof(salesArray) / sizeof(salesArray[0]);
-	// qsort(salesArray, numElements, sizeof(float), sortFloats);
-
 	int monthNum[12] = {0,1,2,3,4,5,6,7,8,9,10,11};
 	for (int i = 0; i < 12; i++)
 	{
